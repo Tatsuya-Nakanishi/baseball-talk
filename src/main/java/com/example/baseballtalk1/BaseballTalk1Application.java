5589fa6df821fee914a13bb45eafd8d8a50aac2b
Tatsuya Nakanishi <nakanishi.tatsuya.onet@gmail.com>
2021-06-18 05:24:57 +0900

    初回コミット

---
+++ src/main/java/com/example/baseballtalk1/BaseballTalk1Application.java
@@ -0,0 +1,13 @@
+package com.example.baseballtalk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballTalk1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballTalk1Application.class, args);
+    }
+
+}
